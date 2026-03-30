7480d7ee54a3efee24cd41d336e83e19239e3fdd
Junior Sousa <juniorsousacmd@gmail.com>
2026-03-30 12:50:50 -0300

    Upload backend folder (bypass)

--- backend/niochat/settings.py
+++ backend/niochat/settings.py
@@ -18,7 +18,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'default-desenvolvimento-niochat')
+SECRET_KEY = 'django-insecure-acK0Z6lOzI_g0tQhb_Ir9bisTC8dZQrTaoIPgBpmjqRzye1XaVsPBXgF2LbhkkbPPAU'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
