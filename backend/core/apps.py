c234b5df41a800ecdddc00028586f3c075f2f8ba
Junior Sousa <juniorsousacmd@gmail.com>
2026-04-05 20:35:36 -0300

    chore(dist): clean branch for client distribution (mobile + workflows)

--- backend/core/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-from .firebase_config import initialize_firebase
-
-class CoreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'
-
-    def ready(self):
-        initialize_firebase()
